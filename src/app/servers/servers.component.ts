cfb5c18fcd96d3c162abd47e411e0fbbec9f4e96
amisha_soni14 <amishas@shriffle.com>
2022-06-27 15:03:49 +0530

    Implementation of view encapsulation & lifecycle hooks

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { Component, OnInit, OnDestroy, AfterViewInit} from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
+@Component ({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  encapsulation : ViewEncapsulation.ShadowDom
 })
-export class ServersComponent implements OnInit {
+export class ServersComponent implements OnInit , OnDestroy, AfterViewInit {
   allowServer = false;
   serverCreate = "No server created!!";
-  serverName = "TestServer";
+  serverName : string = "TestServer";
   serverStatus = false;
 
   constructor() {
@@ -20,6 +21,15 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log("ngOnInit is called")
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy is called")
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit is callled")
   }
 
   onCreate() {
@@ -29,6 +39,11 @@ export class ServersComponent implements OnInit {
 
   onUpdate(event : Event) {
     this.serverName = (<HTMLInputElement>event.target).value
+  }
+
+  OnDestroyMethod() {
+    console.log("OnDestory is callled")
+    console.log( this.serverName.slice(0));
   }
 
 }
